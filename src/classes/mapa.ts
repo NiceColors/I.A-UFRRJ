8d36313c679b4c258ca0d9f8fa237b58a6da63c6
David dos Santos Machado <david_machado@ufrrj.br>
2023-05-12 00:25:25 -0300

    Inserindo robô e meta na matriz

--- src/classes/mapa.ts
+++ src/classes/mapa.ts
@@ -5,6 +5,11 @@ export class Mapa {
     private readonly qtdColunas: number;
     private matriz: Array<Array<EstadoCelula>> = [];
 
+    // TODO: Parametrizar os atributos abaixo a partir de uma interface com o usuário
+    public readonly COORD_INICIAL = [0, 0];
+    public readonly COORD_FINAL = [4, 4];
+    public readonly PERCENTUAL_OBSTACULOS = 20;
+
     constructor(linhas: number, colunas: number) {
         this.qtdLinhas = linhas;
         this.qtdColunas = colunas;
@@ -18,6 +23,9 @@ export class Mapa {
                 this.matriz[l][c] = EstadoCelula.Vazia;
             }
         }
+
+        this.matriz[this.COORD_INICIAL[0]][this.COORD_INICIAL[1]] = EstadoCelula.Robo;
+        this.matriz[this.COORD_FINAL[0]][this.COORD_FINAL[1]] = EstadoCelula.Meta;
     }
 
 }
