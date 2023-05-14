972c4588e00b7a9c0feb35c6fa81c8efd8563fd5
David dos Santos Machado <david_machado@ufrrj.br>
2023-05-14 01:49:16 -0300

    Ajustando enum para rotacionar o objeto com css

--- src/enums/direcao.ts
+++ src/enums/direcao.ts
@@ -1,10 +1,10 @@
 export enum Direcao {
-    Esquerda,
-    DiagonalEC,
-    Cima,
-    DiagonalDC,
-    Direita,
-    DiagonalDB,
-    Baixo,
-    DiagonalEB
+    Cima = 0,
+    DireitaCima = 45,
+    Direita = 90,
+    DireitaBaixo = 135,
+    Baixo = 180,
+    EsquerdaBaixo = 225,
+    Esquerda = 270,
+    EsquerdaCima = 315,
 }
